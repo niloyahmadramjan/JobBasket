46957c32c3bafd196e737ba7f808461e2ed28c03
niloyahmadramjan <niloyahmadramjan@gmail.com>
2025-05-31 17:21:37 +0800

    created jobPost page

--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -98,7 +98,7 @@ const Navbar = () => {
                   Contact
                 </NavLink>
               </li>
-            {user?   <li className="hover:pb-2 transition-all duration-300 ease-in-out hover:text-primary hover:underline ">
+            {!user? "":  <li className="hover:pb-2 transition-all duration-300 ease-in-out hover:text-primary hover:underline ">
                 <NavLink
                   to="/appliedJobs"
                   className={({ isActive }) =>
@@ -107,7 +107,7 @@ const Navbar = () => {
                 >
                   Applied Jobs
                 </NavLink>
-              </li>: ""}
+              </li>}
             </ul>
           </div>
           <div className="navbar-end">
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -8,44 +8,83 @@ import PrivateRoute from "../private/PrivateRoute";
 import JobDetails from "../features/jobs/JobDetails";
 import Profile from "../pages/Profile";
 import AppliedJobs from "../features/jobs/AppliedJobs";
+import JobPost from "../features/employers/pages/JobPost";
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: RootLayout,
     children: [
-        {
-            index: true,
-            Component: Home,
-        },
-        {
-            path: '/register',
-            Component: Register
-        },
-        {
-            path: '/login',
-            Component: Login
-        },
-        {
-            path: '/applyJob/:id',
-            loader: ({params})=> fetch(`http://localhost:3000/jobs/${params.id}`),
-            hydrateFallbackElement: <span>Loading..</span>,
-           element: <PrivateRoute><ApplyJob></ApplyJob></PrivateRoute>,
-        },
-        {
-            path: '/jobDetails/:id',
-             loader: ({params})=> fetch(`http://localhost:3000/jobs/${params.id}`),
-           element: <JobDetails></JobDetails>,
-        },
-        {
-            path: "/profile",
-            Component: Profile,
-        },
-        {
-            path: '/appliedJobs',
-            Component: AppliedJobs,
-        }
-    ]
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/register",
+        Component: Register,
+      },
+      {
+        path: "/login",
+        Component: Login,
+      },
+      {
+        path: "/applyJob/:id",
+        loader: ({ params }) =>
+          fetch(`http://localhost:3000/jobs/${params.id}`),
+        hydrateFallbackElement: <span>Loading..</span>,
+        element: (
+          <PrivateRoute>
+            <ApplyJob></ApplyJob>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/jobDetails/:id",
+        loader: ({ params }) =>
+          fetch(`http://localhost:3000/jobs/${params.id}`),
+        element: <JobDetails></JobDetails>,
+      },
+      {
+        path: "/profile",
+        Component: Profile,
+      },
+      {
+        path: "/appliedJobs",
+      element: (
+          <PrivateRoute>
+            <AppliedJobs></AppliedJobs>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/findajob",
+        element: <p>Find a job page not cooke yet</p>,
+      },
+      {
+        path: "/recruiters",
+        element: <p>Recruiters page not cooke yet</p>,
+      },
+      {
+        path: "/candidates",
+        element: <p>Candidates page not cooke yet</p>,
+      },
+      {
+        path: "/blog",
+        element: <p>b Blog page not cooke yet</p>,
+      },
+      {
+        path: "/contact",
+        element: <p>Contact page not cooke yet</p>,
+      },
+      {
+        path: "/jobPost",
+      element: (
+          <PrivateRoute>
+            <JobPost></JobPost>
+          </PrivateRoute>
+        )
+      }
+    ],
   },
 ]);
 
